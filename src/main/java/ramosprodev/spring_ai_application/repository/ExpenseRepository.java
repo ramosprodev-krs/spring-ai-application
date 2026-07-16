de66157bb44604402e2ad3b80651ca92c850dfec
kauerds <ramosprodev@gmail.com>
2026-07-16 15:11:41 -0300

    Create new method for better Expense search

--- src/main/java/ramosprodev/spring_ai_application/repository/ExpenseRepository.java
+++ src/main/java/ramosprodev/spring_ai_application/repository/ExpenseRepository.java
@@ -4,7 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import ramosprodev.spring_ai_application.entity.Expense;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ExpenseRepository extends JpaRepository<Expense, Long> {
     List<Expense> findByUser_Id(Long userId);
+    Optional<Expense> findByIdAndUser_Id(Long expenseId, Long userId);
 }
